62be2e9597e3698244fdb4137394e3290c1cd897
jonathanbulkeley <jonathan.bulkeley@gmail.com>
2026-02-04 11:32:10 +0100

    Update and rename liveoracle_liquidity.py to liveoracle_btcusd_liquidity.py
    clarified BTCUSD

--- liveoracle_btcusd_liquidity.py
+++ liveoracle_btcusd_liquidity.py
@@ -1,4 +1,4 @@
-# liveoracle_liquidity.py
+# liveoracle_btcusd_liquidity.py
 """
 Live BTCUSD Liquidity-Weighted Oracle
 """
@@ -70,4 +70,5 @@ def paid(i:str):
 
 if __name__ == "__main__":
     port = int(sys.argv[1]) if len(sys.argv)>1 else 8001
+
     uvicorn.run(app, host="127.0.0.1", port=port)
