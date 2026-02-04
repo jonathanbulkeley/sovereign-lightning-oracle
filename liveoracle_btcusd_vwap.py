c2455d898cc55e57da8e1316909a8284e94ed3ae
jonathanbulkeley <jonathan.bulkeley@gmail.com>
2026-02-04 11:34:39 +0100

    Update and rename liveoracle_vwap.py to liveoracle_btcusd_vwap.py

--- liveoracle_btcusd_vwap.py
+++ liveoracle_btcusd_vwap.py
@@ -1,4 +1,4 @@
-# liveoracle_vwap.py
+# liveoracle_btcusd_vwap.py
 """
 Live BTCUSD VWAP Oracle (5-minute window)
 """
@@ -72,4 +72,5 @@ def paid(i:str):
 
 if __name__=="__main__":
     port=int(sys.argv[1]) if len(sys.argv)>1 else 8002
+
     uvicorn.run(app, host="127.0.0.1", port=port)
